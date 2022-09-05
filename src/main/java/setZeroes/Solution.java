d6e80ef4f24771f6067b046a56e474d8358d2524
lizhu129 <lizhu129@gmail.com>
2022-09-05 15:03:04 +0200

    added new exercises

--- src/main/java/setZeroes/Solution.java
+++ src/main/java/setZeroes/Solution.java
@@ -1,7 +1,31 @@
 package setZeroes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public void setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
 
 
