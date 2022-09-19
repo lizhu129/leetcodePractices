6d77ccb2c6f6fb0241a28a625f11bd3b9156aaff
lizhu129 <lizhu129@gmail.com>
2022-09-19 18:02:18 +0200

    added new exercises

---
+++ src/main/java/countOdds/Solution.java
@@ -0,0 +1,16 @@
+package countOdds;
+
+public class Solution {
+    public int countOdds(int low, int high) {
+        int n = high - low + 1;
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            if (low % 2 == 0) {
+                return n / 2;
+            } else {
+                return n / 2 + 1;
+            }
+        }
+    }
+}
