d250aa4d2aeafd7992aeb602654555d64355aa0b
lizhu129 <lizhu129@gmail.com>
2022-10-08 10:53:09 +0200

    Added new exercises

---
+++ src/main/java/numSubarrayProductLessThanK/Solution.java
@@ -0,0 +1,25 @@
+package numSubarrayProductLessThanK;
+
+public class Solution {
+    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 5, 2, 6};
+        int k = 100;
+        System.out.println(numSubarrayProductLessThanK(nums, k));
+    }
+}
