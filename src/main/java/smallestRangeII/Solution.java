32e14f2a3d7689e26eb43723d259c80dde01dd1c
lizhu129 <lizhu129@gmail.com>
2022-10-14 09:59:35 +0200

    Added new exercises

---
+++ src/main/java/smallestRangeII/Solution.java
@@ -0,0 +1,19 @@
+package smallestRangeII;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int smallestRangeII(int[] nums, int k) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int ans = nums[len - 1] - nums[0];
+
+        for (int i = 0; i < len - 1; i++) {
+            int a = nums[i], b = nums[i + 1];
+            int high = Math.max(nums[len - 1] - k, a + k);
+            int low = Math.min(nums[0] + k, b - k);
+            ans = Math.min(ans, high - low);
+        }
+        return ans;
+    }
+}
