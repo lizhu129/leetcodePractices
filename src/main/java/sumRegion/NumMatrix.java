4e260086b2ede1fae3dbb5384e61d48c886c936d
lizhu129 <lizhu129@gmail.com>
2022-10-14 09:48:42 +0200

    Added new exercises

---
+++ src/main/java/sumRegion/NumMatrix.java
@@ -0,0 +1,22 @@
+package sumRegion;
+
+public class NumMatrix {
+
+    int[][] preSum;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        if (m == 0 || n == 0) return;
+        preSum = new int[m + 1][n + 1];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] + matrix[i][j] - preSum[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1];
+    }
+}
