2614d5be5b55f32412432d6694f4e12e20ef2806
lizhu129 <lizhu129@gmail.com>
2022-11-07 10:34:52 +0100

    Added new exercises

--- src/main/java/sortArrayByParityII/Solution.java
+++ src/main/java/sortArrayByParityII/Solution.java
@@ -4,25 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public int[] sortArrayByParityII(int[] nums) {
-        int len = nums.length;
-        List<Integer> evens = new ArrayList<>();
-        List<Integer> odds = new ArrayList<>();
-        for (int n : nums) {
-            if (n % 2 == 0) {
-                evens.add(n);
-            } else {
-                odds.add(n);
-            }
-        }
-        int[] res = new int[len];
-        for (int i = 0; i < len; i++) {
-            if (i % 2 == 0) {
-                res[i] = evens.get(i / 2);
-            } else {
-                res[i] = odds.get(i / 2);
-            }
-        }
-        return res;
-    }
+
 }
