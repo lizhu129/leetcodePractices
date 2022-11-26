45c3bae842e03e55f17eb1d152d02bc63b33d9d4
lizhu129 <lizhu129@gmail.com>
2022-11-26 14:49:52 +0100

    Added new exercises

---
+++ src/main/java/removeDuplicatess/Solution2.java
@@ -0,0 +1,17 @@
+package removeDuplicatess;
+
+public class Solution2 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return n;
+        }
+        int i = 2;
+        for (int j = i; j < n; j++) {
+            if (nums[j] != nums[i - 2]) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
